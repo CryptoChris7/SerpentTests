1595a92615b99ea0af886f7e55eaf6e5ac893581
Chris Calderon <calderon.christian760@gmail.com>
2016-06-08 22:04:05 -0700

    Removed old commented code

--- tests/test_ContractTester.py
+++ tests/test_ContractTester.py
@@ -23,29 +23,3 @@ class TestContractTester(object):
         tester.echo_sender(expects=a1.as_int,
                            asserts=Assert.equal,
                            kwds={'sender': a1.privkey})
-
-        
-# def main():
-    
-#     t = ContractTester(code)
-
-#     print 'Contract code:'
-#     print '--------------'
-#     print code
-#     print '--------------'
-#     print 'Gas cost:', t.gas_cost
-#     print '--------------'
-#     print 'Contract address:', str(t.contractAccount)
-#     print '--------------'
-    
-#     a0 = t.accounts[0].address_as_int
-#     a1 = t.accounts[1].address_as_int
-#     k1 = t.accounts[1].privkey
-
-#     # t.run_tests([('foo', (), {}, "foo"),
-#     #              ('double', (3,), {}, 6),
-#     #              ('echo_sender', (), {}, a0),
-#     #              ('echo_sender', (), {'sender': k1}, a1)])
-
-# if __name__ == '__main__':
-#     main()
